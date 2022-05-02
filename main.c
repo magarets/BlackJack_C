b98b9b1e0e21a7c5556913847084d8c5e348fcb6
yoonan <alswn81616@gmail.com>
2022-05-02 20:17:55 +0900

    modify main.c add print

--- main.c
+++ main.c
@@ -2,18 +2,36 @@
 
 char DECK[52] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K'}; //스(0~12)다(13~25)하(26~38)클(39~51)
 
+void printMain() {
+
+    printf("┏┓ ┏┓                  \n");
+    printf("┃┃ ┃┃      ┏┓        ┏┓    \n");
+    printf("┃┗━┫┃┏━━┳━━┫┃┏┳┳━━┳━━┫┃┏┓\n");
+    printf("┃┏┓┃┃┃┏┓┃┏━┫┗┛┃┃┏┓┃┏━┫┗┛┛\n");
+    printf("┃┗┛┃┗┫┏┓┃┗━┫┏┓┃┃┏┓┃┗━┫┏┓┓\n");
+    printf("┗━━┻━┻┛┗┻━━┻┛┗┫┣┛┗┻━━┻┛┗┛\n");
+    printf("             ┏┛┃         \n");
+    printf("             ┗━┛         \n");
+    printf(" 1. Start \n");
+    printf(" 2. End   \n");
+    printf(" input Number : ");
+}
+
 int main() {
     char n;
-    /*
-    printf("<----Black Jack---->\n\n");
-    printf("1. Start\n");
-    */
-    n = 1;
+
+    printMain();
 
     while(1){
-        gets(&n);
+        scanf("%c", &n);
         if (n == '1') {
             start(DECK);
+        }
+        if (n == '2') {
+            return 0;
+        }
+        else {
+            printf("try again!\n");
         }
     }
     printf("end");
